39d53c41b06e3f4277b7c6a9f612a560cd104f7c
N0mercy00 <dkdhehesp@naver.com>
2021-01-16 20:03:39 +0900

    Intent 및 데이터 전송 예제

--- app/src/main/java/com/example/study00/MainActivity.java
+++ app/src/main/java/com/example/study00/MainActivity.java
@@ -2,31 +2,37 @@ package com.example.study00;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class MainActivity extends AppCompatActivity {
 
-
-    EditText et_id;//변수선언
-    Button btn_test;
+    private Button btn_move;
+    private EditText et_test;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //xml에서 만들었던 것들이랑 연동
-        et_id=findViewById(R.id.et_id);
-        btn_test=findViewById(R.id.btn_test);
+        et_test = findViewById(R.id.et_test);
 
-        btn_test.setOnClickListener(new View.OnClickListener() {
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                et_id.setText("Edit Text 및 버튼연동 예제");
+                str=et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str",str);
+                startActivity(intent);
             }
         });
+
     }
 }
