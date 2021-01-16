bbb00dd6b352be9c83e9d81c88a5412af6efd004
N0mercy00 <dkdhehesp@naver.com>
2021-01-16 18:54:41 +0900

    EditText 및 Button 연동 예제

--- app/src/main/java/com/example/study00/MainActivity.java
+++ app/src/main/java/com/example/study00/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.study00;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText et_id;//변수선언
+    Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //xml에서 만들었던 것들이랑 연동
+        et_id=findViewById(R.id.et_id);
+        btn_test=findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_id.setText("Edit Text 및 버튼연동 예제");
+            }
+        });
     }
 }
