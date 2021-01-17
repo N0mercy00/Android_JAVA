719838beb51297de011829761c984b9c1c55844e
N0mercy00 <dkdhehesp@naver.com>
2021-01-17 13:15:27 +0900

    ImageView and ToastMessage

--- app/src/main/java/com/example/study00/MainActivity.java
+++ app/src/main/java/com/example/study00/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_move;
     private EditText et_test;
     private String str;
+    private ImageView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
                 intent.putExtra("str",str);
                 startActivity(intent);
+            }
+        });
+
+        test=(ImageView)findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"이것이 toast",Toast.LENGTH_SHORT).show();
             }
         });
 
