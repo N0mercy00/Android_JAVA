9c086055b519347bb07fa31a5b08c1e94d2ec846
N0mercy00 <dkdhehesp@naver.com>
2021-01-17 14:25:55 +0900

    ListView example1

--- app/src/main/java/com/example/study00/MainActivity.java
+++ app/src/main/java/com/example/study00/MainActivity.java
@@ -1,49 +1,35 @@
 package com.example.study00;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_move;
-    private EditText et_test;
-    private String str;
-    private ImageView test;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_test = findViewById(R.id.et_test);
+        list = (ListView) findViewById(R.id.list);
+        List<String> data = new ArrayList<>();//데이터를 저장할 리스트 생성
 
-
-        btn_move = findViewById(R.id.btn_move);
-        btn_move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str=et_test.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("str",str);
-                startActivity(intent);
-            }
-        });
-
-        test=(ImageView)findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"이것이 toast",Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        //리스트와, 리스트 뷰를 연결해주는 어뎁터 필요.어댑터를 생성해준다.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        //어뎁터를 연결해준다.
+        list.setAdapter(adapter);
+        //아이템을 추가해준다.
+        data.add("item1");
+        data.add("item2");
+        data.add("item3");
+        adapter.notifyDataSetChanged();
     }
+
 }
