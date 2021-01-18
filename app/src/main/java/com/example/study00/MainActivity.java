9477219347c71b1e5b5408205f8a9ae8d247da19
N0mercy00 <dkdhehesp@naver.com>
2021-01-18 15:40:34 +0900

    SharedPreferences 임시값 저장예제 1

--- app/src/main/java/com/example/study00/MainActivity.java
+++ app/src/main/java/com/example/study00/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.study00;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,25 +14,27 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView list;
+    private EditText et_save;
+    String shared="file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        list = (ListView) findViewById(R.id.list);
-        List<String> data = new ArrayList<>();//데이터를 저장할 리스트 생성
-
-        //리스트와, 리스트 뷰를 연결해주는 어뎁터 필요.어댑터를 생성해준다.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
-        //어뎁터를 연결해준다.
-        list.setAdapter(adapter);
-        //아이템을 추가해준다.
-        data.add("item1");
-        data.add("item2");
-        data.add("item3");
-        adapter.notifyDataSetChanged();
+        et_save = (EditText) findViewById(R.id.et_save);
+        SharedPreferences sharedPreferences = getSharedPreferences(shared,0);
+        String value = sharedPreferences.getString("sample","");
+        et_save.setText(value);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sharedPreferences = getSharedPreferences(shared,0);
+        SharedPreferences.Editor editor =sharedPreferences.edit();
+        String value = et_save.getText().toString();
+        editor.putString("sample",value);
+        editor.commit();
+    }
 }
